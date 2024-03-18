336080aedc9b2f8d5876096437631ab98956ab71
Nazan Korkmaz <nazzankorkmazz@gmail.com>
2024-03-18 18:28:07 +0300

    SignInFragment'e ve PostAtmaFragment'e galeriye giriş izinleri verildi

--- app/src/main/java/com/example/tasarmprojesi/PostAtmaFragment.kt
+++ app/src/main/java/com/example/tasarmprojesi/PostAtmaFragment.kt
@@ -1,13 +1,24 @@
 package com.example.tasarmprojesi
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import com.example.tasarmprojesi.databinding.FragmentPostAtmaBinding
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
@@ -19,7 +30,9 @@ class PostAtmaFragment : Fragment() {
     private var _binding: FragmentPostAtmaBinding? = null
     private val binding get() = _binding!!
 
-
+    private lateinit var  activityResultLauncher : ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher : ActivityResultLauncher<String>
+    var selectedPicture : Uri? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -38,14 +51,58 @@ class PostAtmaFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        registerLauncher()
+
         binding.shareButton.setOnClickListener{
             findNavController().navigate(R.id.action_postAtmaFragment_to_postFragment)
 
         }
 
+        binding.postImageView.setOnClickListener {
+            //openGallery()
+            if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.READ_MEDIA_IMAGES)) {
+                    Snackbar.make(view, "Permission needed for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission",
+                        View.OnClickListener {
+                            permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                        }).show()
+                } else {
+                    permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                }
+            } else {
+                val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+
+            }
+        }
+
     }
 
+    private  fun registerLauncher(){
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if(result.resultCode == AppCompatActivity.RESULT_OK){
+                val intentFromResult = result.data
+                if (intentFromResult != null){
+                    selectedPicture =   intentFromResult.data
+                    selectedPicture?.let{
+                        binding.postImageView.setImageURI(it)
+                    }
+                }
+            }
+        }
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){ result ->
+            if(result){
+                //permission granted
+                val intentToGallery= Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+            }
+            else{
+                //permission denied
+                Toast.makeText(requireContext(),"Permission needed!", Toast.LENGTH_LONG).show()
+            }
 
+        }
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/example/tasarmprojesi/SignInFragment.kt
+++ app/src/main/java/com/example/tasarmprojesi/SignInFragment.kt
@@ -1,16 +1,27 @@
 package com.example.tasarmprojesi
 
+import android.Manifest
+import android.app.Activity
 import android.app.DatePickerDialog
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.DatePicker
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.tasarmprojesi.databinding.FragmentSignInBinding
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
@@ -19,12 +30,19 @@ import java.util.Calendar
 import java.util.Locale
 
 
+
 class SignInFragment : Fragment()  {
     private var _binding: FragmentSignInBinding? = null
     private val binding get() = _binding!!
+    companion object {
+        private const val PICK_IMAGE_REQUEST = 1
+    }
 
     private lateinit var auth: FirebaseAuth
 
+    private lateinit var  activityResultLauncher : ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher : ActivityResultLauncher<String>
+    var selectedPicture : Uri? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -37,6 +55,10 @@ class SignInFragment : Fragment()  {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        registerLauncher()
+
+
 
         binding.editTextDOB.setOnClickListener {
             showDatePickerDialog()
@@ -65,7 +87,29 @@ class SignInFragment : Fragment()  {
             //findNavController().navigate(R.id.action_signInFragment_to_loginFragment)
         }
 
+        binding.imageView.setOnClickListener {
+            //openGallery()
+            if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.READ_MEDIA_IMAGES)) {
+                    Snackbar.make(view, "Permission needed for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission",
+                        View.OnClickListener {
+                            permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                        }).show()
+                } else {
+                    permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                }
+            } else {
+                val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+
+            }
+        }
+
     }
+
+
+
+
 
     private fun showDatePickerDialog() {
         val calendar = Calendar.getInstance()
@@ -87,7 +131,50 @@ class SignInFragment : Fragment()  {
         datePickerDialog.show()
     }
 
+    /*
+    private fun openGallery() {
+        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST)
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
+            val selectedImageUri = data.data
+
+            // Set the background of the ImageButton with the selected image URI
+            binding.imageView.background = null
+            binding.imageView.setImageURI(selectedImageUri)
+        }
+    }
+    */
+
+    private  fun registerLauncher(){
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if(result.resultCode == AppCompatActivity.RESULT_OK){
+                val intentFromResult = result.data
+                if (intentFromResult != null){
+                    selectedPicture =   intentFromResult.data
+                    selectedPicture?.let{
+                        binding.imageView.setImageURI(it)
+                    }
+                }
+            }
+        }
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){ result ->
+            if(result){
+                //permission granted
+                val intentToGallery= Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+            }
+            else{
+                //permission denied
+                Toast.makeText(requireContext(),"Permission needed!", Toast.LENGTH_LONG).show()
+            }
+
+        }
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
