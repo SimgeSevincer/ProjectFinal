89ee06d81757bf665c3f235b673defd075636c22
Ezgi Adıgüzel <ezgiadiguzel01@gmail.com>
2024-05-25 21:22:33 +0300

    Yeni makale eklendi ve anasayfa düzeltildi

--- app/src/main/java/com/example/tasarmprojesi/model/ArticleData.kt
+++ app/src/main/java/com/example/tasarmprojesi/model/ArticleData.kt
@@ -19,7 +19,7 @@ class ArticleData(context: Context) {
         Article(
             "2",
             "Sağlık",
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVDEcL5p-vsbFHSFujIhLAu4q6xSYPFyo7mb6sAghrHQ&s",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-ajfWl2m_S5CikL0kB47PK5nXGuRXKcPUgw&s",
             "İsveç Diyeti",
             "Sabah kahvaltılarını birkaç günün dışında genel olarak sütsüz ya da bir çay bardağı yağsız süt ile hazırlanmış granül kahvenin oluşturduğu İsveç diyetinde öğlen ve akşam yemeklerinin ilk günlerinde protein kaynağı olarak sadece iki adet haşlanmış yumurta yer alırken ilerleyen günlerde yerini ağırlıklı olarak et, balık ve tavuk ürünlerinin yanında sınırsız olarak alınabilen az yağlı salata oluşturur. Meyve grubu ürünleri belirtildiği üzere ve miktarda alınmalıdır.",
             "2024-02-28",
@@ -33,6 +33,15 @@ class ArticleData(context: Context) {
             "Spora başlamak için yürüyüş, koşu gibi aktiviteler veya fitness salonlarını tercih edebilirsiniz. Sporu devamlı hale getirmek ve rutin bir parça yapmak önemlidir. Zamanı doğru ayarlayarak, uygun aktiviteler seçmek önemlidir. Egzersiz seçerken kişisel fiziksel özelliklerinizi ve keyif aldığınız aktiviteleri göz önünde bulundurun. Motivasyonu artırmak için grup derslerine katılabilir veya eğlenceli aktiviteler tercih edebilirsiniz.",
             "2024-03-27",
             15
+        ),
+        Article(
+            "4",
+            "Sağlık",
+            "https://www.kidsgourmet.com.tr/wp-content/uploads/2018/08/%C3%87%C3%B6lyak-d%C4%B1%C5%9F%C4%B1-gl%C3%BCten-696x461.jpg",
+            "Glutensiz Diyet",
+            "Toplumda işlenmiş gıdaların hayatımızdan çıkarılmasını söylerken gluteni hayatımızdan çıkardığımızda daha kolay zayıflama algısı ya da daha sağlıklı olma düşüncesi tamamen yanlıştır. Sağlıklı bir kişinin glütensiz beslenmesi yarar yerine ileriki dönemde kişiye zarar vermektedir",
+            "2024-05-25",
+            12
         )
     )
 
