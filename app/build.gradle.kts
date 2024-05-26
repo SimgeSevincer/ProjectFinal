8b50809dc00f1acced50731cb4564d34b5921b76
Nazan Korkmaz <nazzankorkmazz@gmail.com>
2024-05-26 19:38:16 +0300

    gmail giris - google-services dosyasi yeniden yuklendi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     //id("com.google.gms.google-services")
     id("com.google.gms.google-services")
+    id("androidx.navigation.safeargs.kotlin")
     id ("kotlin-parcelize")
 
 }
@@ -43,6 +44,7 @@ android {
 }
 
 dependencies {
+    implementation("com.google.firebase:firebase-auth:23.0.0")
     val nav_version = "2.7.5"
 
     // Kotlin
@@ -64,9 +66,9 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:$nav_version")
 
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -81,5 +83,9 @@ dependencies {
     implementation("com.google.firebase:firebase-storage-ktx")
 
     implementation ("com.squareup.picasso:picasso:2.8")
+
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
+    implementation("androidx.credentials:credentials:1.3.0-alpha04")
+    implementation("androidx.credentials:credentials-play-services-auth:1.3.0-alpha04")
 
 }
